c638f86fadeb57b43388632ae31309430aa33304
LUCAS PAIVA FORTE <lucaspaivacedro@hotmail.com>
2018-09-25 20:24:27 -0300

    AvaleiacaoTopicosEspeciais ADS-FVS AV1

---
+++ AV1/AvaliacaoTopicosEspeciais/src/main/java/eleicao/resources/CategoriaResource.java
@@ -0,0 +1,28 @@
+package eleicao.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import eleicao.domain.Candidato;
+import eleicao.service.CandidatoService;
+
+@RestController
+@RequestMapping(value="/candidatos")
+public class CategoriaResource {
+	
+	@Autowired
+	private CandidatoService service;
+		
+		@RequestMapping(value="/{id}", method=RequestMethod.GET)
+		public ResponseEntity<?> find(@PathVariable Integer id){
+			Candidato cand = service.buscar(id);
+			
+			return ResponseEntity.ok().body(cand);
+			
+		}
+		
+	}
